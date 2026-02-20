9e25249b22b8fa81e26255798de7d351acf31fe8
Olofin Evelyn <olofinevelyn@gmail.com>
2026-02-20 20:41:41 +0100

    checkout edit

--- src/App.tsx
+++ src/App.tsx
@@ -238,7 +238,7 @@ function AppContent() {
 
 
   const inputClass =
-  "w-full border border-gray-300 rounded-lg px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-blue-400 bg-white text-black";
+  "w-full min-w-0 border border-gray-300 rounded-lg px-3 py-2 text-base outline-none focus:ring-2 focus:ring-blue-400 bg-white text-black";
 
   const labelClass = "block text-sm font-medium text-gray-700 mb-1";
 
